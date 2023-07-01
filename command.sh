910e369da8873e64f72a3e047e6a688edfc89bc6
erangipelawage <iit19059@std.uwu.ac.lk>
2023-07-02 01:49:14 +0530

    2023-07-02 commit

---
+++ command.sh
@@ -0,0 +1,18 @@
+# backend project creation
+django-admin startproject backend
+
+# frontend project creation
+npx create-react-app frontend
+
+# mysql client install
+pip install mysqlclient
+
+# django project run
+python manage.py runserver
+
+# database migration
+python manage.py makemigrations
+python manage.py migrate
+
+
+
