110ddc9499d3824dcf5980cce7ed1376ff736b15
Lasantha Sanjeewa Silva <41699526+sanju2@users.noreply.github.com>
2023-11-18 00:01:04 +0400

    2023-11-18 v1 commit

--- hotel_reservation_site/urls.py
+++ hotel_reservation_site/urls.py
@@ -19,8 +19,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('hotel/', include("hotel_app.urls")),
     path('api/', include("rest_framework.urls")),
-    path('accounts/', include('accounts.urls')),
-    path('payments/', include('payments.urls'))
+    path('accounts/', include('accounts.urls'))
 
 ]
 
